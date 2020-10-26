d7c1bd026c7ba2cec92b6dc086aeca1c167b4269
LakshanDinesh.com <lakshandinesh90@gamil.com>
2020-10-26 18:23:20 +0530

    Add first four files

---
+++ datatypes_variables.py
@@ -0,0 +1,31 @@
+#INTEGERS
+x=10    #as a
+
+#STRINGS
+x="thanuri" #as a string
+print(x)    # last assigned value will be printed.
+
+#variables,
+   # cant be defined,start with number, start with _ or A-Z
+   # case sesitive age<>Age<>AGE
+#
+
+a,b,c = "lakshan", "dinesh", "liyanarachchi"
+print(a)
+print(b)
+print(c)
+
+
+p=q=r="amma"
+print(p)
+print(q)
+print(r)
+
+""""x=5
+y="lakshan"
+print(x+y)      can't disply int and str addtion"""
+
+
+
+
+
---
+++ indentation.py
@@ -0,0 +1,5 @@
+if 10<20:
+    print("the ten is lower than towenty")
+    #indentation is very important unless it may be syntax error.
+    """if 5>2:
+        print("the five is greater than tow")"""
